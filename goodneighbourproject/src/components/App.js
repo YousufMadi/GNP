90e301061d0b8722943b8d1aff6244e4611872b5
Ayub <ayub.hassan97@gmail.com>
2020-06-14 23:48:51 -0400

    Added Routing functionality, created page components

--- goodneighbourproject/src/components/App.js
+++ goodneighbourproject/src/components/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Feed from "./Feed";
+import Home from "./Home";
+import history from "../history";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router history={history}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/feed" exact component={Feed} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ goodneighbourproject/src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Feed extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Feed Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Feed;
