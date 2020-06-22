03274a42d540a3f2156b958ab7a94e850503518f
Ani <anirudha.kanodia@hotmail.com>
2020-06-23 00:33:24 +0530

    Make structural changes to the feed page

--- goodneighbourproject/src/components/RequestTimeline/RequestTimeline.js
+++ goodneighbourproject/src/components/RequestTimeline/RequestTimeline.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 import "./requestTimeline.css";
-import RequestContainer from "../RequestContainer/RequestContainer";
+import RequestPost from "../RequestPost/RequestPost";
 
 class RequestTimeline extends React.Component {
+
+  state = {
+    posts:
+      [{ id: 1, name: "Yousuf", size: "Small", reimbursement: "Cash", items: ["Chips", "Apples", "Flour"] },
+      { id: 2, name: "Adham", size: "Large", reimbursement: "cheque" },
+      { id: 3, name: "Omar", size: "Large", reimbursement: "cheque" },
+      { id: 4, name: "Donia", size: "Large", reimbursement: "cheque" },
+      { id: 2, name: "Adham", size: "Large", reimbursement: "cheque" },
+      { id: 2, name: "Adham", size: "Large", reimbursement: "cheque" },
+      { id: 2, name: "Adham", size: "Large", reimbursement: "cheque" }]
+  }
+
   render() {
     return (
-      <div
-        className="rightSide">
-        <RequestContainer />
-      </div>
+        <div className="timeline">
+          {/*<RequestAsk />*/}
+
+          <div className="posts">
+            {this.state.posts.map((post, index) =>
+              <RequestPost key={index} id={post.id} name={post.name} size={post.size} reimbursement={post.reimbursement} />)}
+          </div>
+
+        </div>
     );
   }
 }
