5430e7aed9a53c6fdc32066b36dd8f7f712872fe
Ayub <ayub.hassan97@gmail.com>
2020-06-23 21:08:42 -0400

    Added creating request functionality

--- goodneighbourproject/src/components/GroceryList/ItemsList.js
+++ goodneighbourproject/src/components/GroceryList/ItemsList.js
@@ -1,66 +1,58 @@
 import React from "react";
 import "./groceryList.css";
 import Items from "./Items";
-// import { Col, Row } from "reactstrap";
 
 class ItemsList extends React.Component {
+  state = {
+    items: [],
+  };
+  addItem = (e) => {
+    e.preventDefault();
 
-    constructor(props) {
-        super(props);
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now(),
+      };
 
-        this.state = {
-            items: [],
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem),
         };
+      });
 
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
+      this._inputElement.value = "";
+      this.props.handleItemsChange([...this.state.items, newItem]);
     }
-    addItem(e) {
-        if (this._inputElement.value !== "") {
-            var newItem = {
-                text: this._inputElement.value,
-                key: Date.now()
-            };
+  };
 
-            this.setState((prevState) => {
-                return {
-                    items: prevState.items.concat(newItem)
-                };
-            });
+  deleteItem = (key) => {
+    var filteredItems = this.state.items.filter(function (item) {
+      return item.key !== key;
+    });
 
-            this._inputElement.value = "";
-        }
+    this.setState({
+      items: filteredItems,
+    });
+    this.props.handleItemsChange(filteredItems);
+  };
 
-        console.log(this.state.items);
-
-        e.preventDefault();
-    }
-
-    deleteItem(key) {
-        var filteredItems = this.state.items.filter(function (item) {
-            return (item.key !== key);
-        });
-
-        this.setState({
-            items: filteredItems
-        });
-    }
-
-    render() {
-        return (
-            <div className="itemListMain">
-                <div className="header">
-                    <form onSubmit={this.addItem}>
-                        <input placeholder="enter item" ref={(a) => this._inputElement = a}>
-                        </input>
-                        <button type="submit">+</button>
-                    </form>
-                </div>
-                <Items entries={this.state.items}
-                    delete={this.deleteItem} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="itemListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              placeholder="enter item"
+              ref={(a) => (this._inputElement = a)}
+            ></input>
+            <button type="submit">+</button>
+          </form>
+        </div>
+        <Items entries={this.state.items} delete={this.deleteItem} />
+      </div>
+    );
+  }
 }
 
 export default ItemsList;
