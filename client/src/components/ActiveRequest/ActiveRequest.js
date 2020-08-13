745d1d64878086d5fd9cc37b72528864643c3619
Ayub <ayub.hassan97@gmail.com>
2020-08-13 18:16:33 -0400

    added directions in active request

--- client/src/components/ActiveRequest/ActiveRequest.js
+++ client/src/components/ActiveRequest/ActiveRequest.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import Sidebar from "../Sidebar/Sidebar";
+import Map from "../Timeline/Map";
 import { connect } from "react-redux";
 
 import "../../stylesheets/activerequest.css";
 
 import { completePost } from "../../actions/user";
+
+const keys = {
+  key1: "AIzaSyCx3EBDjdwQ4Gb6698FPEWsTB7bNL_o7Ow",
+  key2: "AIzaSyARRBVg-xS1QeLJMfoCSeQm5At4Q-E7luU",
+};
 
 class ActiveRequest extends React.Component {
   state = {
@@ -49,7 +55,17 @@ class ActiveRequest extends React.Component {
                   's Request
                 </h4>
               </div>
-              <div className="map-route"></div>
+              <div className="map-route">
+                <Map
+                  currentUserLocation={this.props.currentUserLocation}
+                  active_post={true}
+                  posts={[this.props.currentUser.active_post]}
+                  googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${keys.key2}`}
+                  loadingElement={<div style={{ height: "100%" }} />}
+                  containerElement={<div style={{ height: "100%" }} />}
+                  mapElement={<div style={{ height: "100%" }} />}
+                />
+              </div>
               <div className="active-items-requested">
                 <label>Items Requested</label>
                 <ul className="request-items-list">{this.renderItems()}</ul>
